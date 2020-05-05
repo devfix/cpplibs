2278ee8426d5d258bd87d9f94ede11378cebfef2
Praktikum <Praktikum@DEV-NB-001>
2020-05-05 16:06:58 +0200

    more tests

--- devfix/dsp/fft.h
+++ devfix/dsp/fft.h
@@ -7,97 +7,79 @@
 #include <cstdint>
 #include <complex>
 
-template<std::size_t BITS, typename T, std::size_t N = (1 << BITS), std::size_t N_2 = (1 << (BITS - 1))>
+template<std::size_t BITS, typename T>
 struct FFT
 {
-
-// user defined values
-#define FFT_SAMPLE_RATE 20000
-#define FFT_AMPLITUDE 64
-
-// other constants
-#define FFT_LEN_2 (1u << (FFT_P - 1u))
-#define FFT_LEN (FFT_LEN_2 << 1u)
-#define FFT_COS_THETA 0.999981175f  // cos(π÷LEN)
-#define FFT_SIN_THETA 0.006135885f  // sin(π÷LEN)
-#define FFT_MAX_MAG (FFT_AMPLITUDE * FFT_AMPLITUDE)
-
-	static constexpr std::complex<T> PHI_T = { cos(M_PI / N), -sin(M_PI / N) };
-
-	static void fft_transform_inplace(std::complex<T>* vect)
+private:
+	template<std::size_t N, std::size_t N_2>
+	struct _FFT_WRAPPED
 	{
-		size_t k = N, n;
-		std::complex<T> phi_t(PHI_T);
-		std::complex<T> t;
-		while (k)
+		static void transform_inplace(std::complex<T>* vect)
 		{
-			n = k;
-			k >>= 1u;
 
-			phi_t *= phi_t;
-
-			t.real(1);
-			t.imag(0);
-			for (size_t l = 0; l < k; l++)
+			size_t k = N, n;
+			std::complex<T> phi_t(PHI_T);
+			std::complex<T> t;
+			while (k)
 			{
-				for (size_t ka = l; ka < N; ka += n)
+				n = k;
+				k >>= 1u;
+
+				phi_t *= phi_t;
+
+				t.real(1);
+				t.imag(0);
+				for (size_t l = 0; l < k; l++)
 				{
-					const size_t kb = ka + k;
-					std::complex<T> diff = vect[ka] - vect[kb];
-					vect[ka] += vect[kb];
-					vect[kb] = diff * t;
+					for (size_t ka = l; ka < N; ka += n)
+					{
+						const size_t kb = ka + k;
+						std::complex<T> diff = vect[ka] - vect[kb];
+						vect[ka] += vect[kb];
+						vect[kb] = diff * t;
+					}
+					t *= phi_t;
 				}
-				t *= phi_t;
+			}
+			order_bit_reversed(vect);
+
+		}
+
+		static void magnitude(std::complex<T>* vect, T* mag)
+		{
+			for (size_t k = 0; k < N; k++)
+			{
+				T re = vect[k].real() / N_2;
+				T im = vect[k].imag() / N_2;
+				mag[k] = re * re + im * im; // skip sqrt of Pythagoras, save squared absolute values directly
 			}
 		}
 
-		fft_order_bit_reversed(vect);
-	}
+	private:
+		static constexpr std::complex<T> PHI_T = { cos(M_PI / N), -sin(M_PI / N) };
 
-	static void fft_magnitude(std::complex<T>* vect, T* mag)
-	{
-		for (size_t k = 0; k < N; k++)
+		static void order_bit_reversed(std::complex<T>* vect)
 		{
-			T re = vect[k].real() / N_2;
-			T im = vect[k].imag() / N_2;
-			mag[k] = re * re + im * im; // skip sqrt of Pythagoras, save squared absolute values directly
-		}
-	}
-
-	static T fft_band(T* mag, size_t a, size_t b)
-	{
-		size_t len = b - a + 1;
-		mag += a;
-		T sum = 0;
-		while (a <= b)
-		{
-			sum += *mag++;
-			a++;
-		}
-		sum /= len;
-		return sum;
-	}
-
-	static void fft_order_bit_reversed(std::complex<T>* vect)
-	{
-		for (size_t a = 0; a < N; a++)
-		{
-			size_t b = fft_bit_reverse(a);
-			if (b > a)
+			for (size_t a = 1; a < N_2; a++)
 			{
+				size_t b = bit_reverse(a);
 				std::complex<T> tmp(vect[a]);
 				vect[a] = vect[b];
 				vect[b] = tmp;
 			}
 		}
-	}
 
-	static size_t fft_bit_reverse(size_t b)
-	{
-		b = (b & 0b1010101010101010u) >> 1u | (b & 0b0101010101010101u) << 1u;
-		b = (b & 0b1100110011001100u) >> 2u | (b & 0b0011001100110011u) << 2u;
-		b = (b & 0b1111000011110000u) >> 4u | (b & 0b0000111100001111u) << 4u;
-		b = (b & 0b1111111100000000u) >> 8u | (b & 0b0000000011111111u) << 8u;
-		return b >> (16u - BITS);
-	}
+		static size_t bit_reverse(size_t b)
+		{
+			b = (b & 0b1010101010101010u) >> 1u | (b & 0b0101010101010101u) << 1u;
+			b = (b & 0b1100110011001100u) >> 2u | (b & 0b0011001100110011u) << 2u;
+			b = (b & 0b1111000011110000u) >> 4u | (b & 0b0000111100001111u) << 4u;
+			b = (b & 0b1111111100000000u) >> 8u | (b & 0b0000000011111111u) << 8u;
+			return b >> (16u - BITS);
+		}
+	};
+
+public:
+	static void transform_inplace(std::complex<T>* vect)
+	{ _FFT_WRAPPED<1 << BITS, 1 << (BITS - 1)>::transform_inplace(vect); }
 };
--- devfix/dsp/test/test_fft.cpp
+++ devfix/dsp/test/test_fft.cpp
@@ -21,7 +21,7 @@ void test_rectangle()
 		{ 16 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }};
 	std::complex<double> vect[] = {
 		{ 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }, { 1 }};
-	FFT<4, double>::fft_transform_inplace(vect);
+	FFT<4, double>::transform_inplace(vect);
 	ASSERT_ARRAY_EQUAL(vect, expected);
 }
 
@@ -32,7 +32,61 @@ void test_dirac()
 		{ 8192 }, { 8192 }, { 8192 }};
 	std::complex<double> vect[] = {
 		{ 8192 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }};
-	FFT<4, double>::fft_transform_inplace(vect);
+	FFT<4, double>::transform_inplace(vect);
+	ASSERT_ARRAY_EQUAL(vect, expected);
+}
+
+void test_periodic()
+{
+	std::complex<double> expected[] = {
+		{ 0 },
+		{ 0.5 },
+		{ 1 },
+		{ 0.5 },
+		{ 0 },
+		{ 0.5 },
+		{ 1 },
+		{ 0.5 },
+	};
+	std::complex<double> vect[] = {
+		{ 0.5 },
+		{ 0 },
+		{ -0.25 },
+		{ 0 },
+		{ 0 },
+		{ 0 },
+		{ -0.25 },
+		{ 0 }
+	};
+	FFT<3, double>::transform_inplace(vect);
+	ASSERT_ARRAY_EQUAL(vect, expected);
+}
+
+void test_random()
+{
+	std::complex<double> expected[] = {
+		{ 360, 0 },
+		{ -40, 97 },
+		{ -40, 40 },
+		{ -40, 17 },
+		{ -40, 0 },
+		{ -40, -17 },
+		{ -40, -40 },
+		{ -40, -97 },
+	};
+	std::complex<double> vect[] = {
+		{ 10 },
+		{ 20 },
+		{ 30 },
+		{ 40 },
+		{ 50 },
+		{ 60 },
+		{ 70 },
+		{ 80 }
+	};
+	FFT<3, double>::transform_inplace(vect);
+	std::transform(std::begin(vect), std::end(vect), std::begin(vect), [](auto c)
+	{ return decltype(c){ std::round(c.real()), std::round(c.imag()) }; });
 	ASSERT_ARRAY_EQUAL(vect, expected);
 }
 
@@ -40,5 +94,7 @@ void test_dsp()
 {
 	test_rectangle();
 	test_dirac();
+	test_periodic();
+	test_random();
 	std::cout << "all tests have passed successfully" << std::endl;
 }
