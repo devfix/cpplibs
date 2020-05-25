1ad07585daf2c2631b3c07178ea72c40a033cda1
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2020-05-25 16:29:51 +0200

    reformatted

--- devfix/dsp/addr.h
+++ devfix/dsp/addr.h
@@ -12,30 +12,15 @@ namespace devfix::dsp
 	struct _bit_reverse;
 
 	template<std::size_t BITS, std::size_t I>
-	struct _bit_reverse<BITS, I, 0>
-	{
-		enum
-		{
-			value = 0
-		};
-	};
+	struct _bit_reverse<BITS, I, 0> { enum { value = 0 }; };
 
 	template<std::size_t BITS, std::size_t I, std::size_t K>
-	struct _bit_reverse
-	{
-		enum
-		{
-			value = (I & (1u << (BITS - K)) ? (1u << (K - 1u)) : 0) + _bit_reverse<BITS, I, K - 1u>::value
-		};
-	};
+	struct _bit_reverse { enum { value = (I & (1u << (BITS - K)) ? (1u << (K - 1u)) : 0) + _bit_reverse<BITS, I, K - 1u>::value }; };
 
 	template<std::size_t N, std::size_t I>
 	struct bit_reversed
 	{
-		enum
-		{
-			value = _bit_reverse<devfix::base::meta::math::log2<N>::value, I, devfix::base::meta::math::log2<N>::value>::value
-		};
+		enum { value = _bit_reverse<devfix::base::meta::math::log2<N>::value, I, devfix::base::meta::math::log2<N>::value>::value };
 	};
 
 }
