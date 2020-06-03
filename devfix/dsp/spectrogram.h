dcb6a2e5e8d3d06eff1a9f423f5a01e1d1c71d9f
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2020-06-03 15:47:20 +0200

    spectrogram with test

--- devfix/dsp/spectrogram.h
+++ devfix/dsp/spectrogram.h
@@ -8,14 +8,16 @@
 #include <cstring>
 #include <complex>
 #include <vector>
+#include <list>
 #include <array>
+#include "fft.h"
 
 #include <iostream>
 
 namespace devfix::dsp
 {
 
-	template<typename FloatT, std::size_t N>
+	template<typename FloatT, std::size_t N, FloatT(* win_fun)(std::size_t)>
 	struct spectrogram
 	{
 		using complex_t = std::complex<FloatT>;
@@ -28,9 +30,9 @@ namespace devfix::dsp
 
 		void add(const complex_t* data, std::size_t len)
 		{
-			std::size_t old_size = data_.size();
-			data_.resize(old_size + len);
-			std::memcpy(data_.data() + sizeof(complex_t) * old_size, data, len);
+			std::size_t old_size = temp_data_.size();
+			temp_data_.resize(old_size + len);
+			std::memcpy(temp_data_.data() + old_size, data, sizeof(complex_t) * len);
 			update();
 		}
 
@@ -39,34 +41,52 @@ namespace devfix::dsp
 			add(vec.data(), vec.size());
 		}
 
-		template <std::size_t N_elems>
+		template<std::size_t N_elems>
 		void add(const std::array<complex_t, N_elems>& arr)
 		{
 			add(arr.data(), N_elems);
 		}
 
+		std::array<complex_t, N> pop()
+		{
+			if (data_.empty()) { throw std::out_of_range("spectrogram empty"); }
+			std::array<complex_t, N> arr = data_.front();
+			data_.pop_front();
+			return arr;
+		}
+
+		std::size_t size() const
+		{
+			return data_.size();
+		}
+
 	private:
 		std::size_t window_distance_;
-		std::vector<complex_t> data_;
+		std::vector<complex_t> temp_data_;
+		std::list<std::array<complex_t, N>> data_;
 
 		void update()
 		{
 			// calc count of new calculatetable ffts
-			std::size_t n = std::floor(double(data_.size() - N + 1) / window_distance_);
-			std::cout << "update: " << n << "\n";
+			std::size_t n = std::floor(double(temp_data_.size() - N + 1) / window_distance_);
 			if (n)
 			{
-				auto ptr = data_.data();
+				auto ptr = temp_data_.data();
 				for (std::size_t k = 0; k++ < n; ptr += window_distance_) { append_fft(ptr); }
 
 				// remove n * window_distance_ elements from beginning of data_
-				std::vector<complex_t>(data_.begin() + n * window_distance_, data_.end()).swap(data_);
+				std::vector<complex_t>(temp_data_.begin() + n * window_distance_, temp_data_.end()).swap(temp_data_);
 			}
 		}
 
 		void append_fft(complex_t* field)
 		{
-			std::cout << "append: " << *field << "\n";
+			auto& window = data_.emplace_back();
+			std::memcpy(window.data(), field, sizeof(complex_t) * N);
+			std::size_t index = 0;
+			auto win_fun_adapt = [&index](const complex_t& complex) -> complex_t { return complex * win_fun(index++); };
+			std::transform(window.begin(), window.end(), window.begin(), win_fun_adapt);
+			fft::transform_inplace<FloatT>(window.data(), N);
 		}
 	};
 
--- devfix/dsp/test/test_spectrogram.cpp
+++ devfix/dsp/test/test_spectrogram.cpp
@@ -5,18 +5,114 @@
 #if ENABLE_GOOGLETEST == 1
 
 #include <gtest/gtest.h>
-#include <cstring>
 #include "../spectrogram.h"
 
 using namespace devfix::dsp;
 
-TEST(Spectrogram, ComplexTest)
+auto rect_win = [](std::size_t k) -> double { return 1; };
+
+constexpr std::size_t FFT_LEN = 8;
+
+template<int digits, typename T>
+constexpr T round(T val) { return std::round(val * std::pow(10, digits)) * std::pow(10, -digits); }
+
+std::array<double, FFT_LEN> get_magnitudes(const std::array<std::complex<double>, FFT_LEN>& win)
 {
-	spectrogram<double, 4> spec(1);
-	spec.add(std::vector<std::complex<double>>{{ 0 }, { 1 }, { 2 }});
-	spec.add(std::vector<std::complex<double>>{{ 3 }, { 4 }, { 5 }});
-	spec.add(std::vector<std::complex<double>>{{ 6 }, { 7 }, { 8 }});
-	spec.add(std::vector<std::complex<double>>{{ 9 }, { 10 }, { 11 }});
+	std::array<double, FFT_LEN> mag;
+	std::transform(win.begin(), win.end(), mag.begin(), std::abs<double>);
+	std::transform(mag.begin(), mag.end(), mag.begin(), round<3, double>);
+	return mag;
+}
+
+#define ASSERT_ARRAYS_EQ(arr1, arr2, len, th) \
+    for(std::size_t i = 0; i < len ; i++) \
+    { \
+        ASSERT_LT(std::abs(arr1[i] - arr2[i]), th); \
+    } \
+    (void) len
+
+TEST(Spectrogram, RectWindow)
+{
+	// generate test data
+	std::array<std::complex<double>, 16> test_data;
+	test_data.fill(0);
+	std::size_t begin_sine_curve = 4;
+	std::size_t end_sine_curve = 12;
+	for (std::size_t i = begin_sine_curve; i < end_sine_curve; i++)
+	{
+		double phi = 2 * M_PI * double(i - begin_sine_curve) / double(end_sine_curve - begin_sine_curve);
+		test_data[i] = std::sin(phi);
+	}
+
+	// create spectrogram and fill it
+	spectrogram<double, FFT_LEN, rect_win> spec(1);
+	for (auto& c : test_data) { spec.add(&c, 1); }
+
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 2.414, 2, 1, 0, 0.414, 0, 1, 2 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 2.414, 2, 1, 0, 0.414, 0, 1, 2 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 1.707, 2.55, 1.225, 0.707, 0.293, 0.707, 1.225, 2.55 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 0.707, 3.536, 0.707, 0.707, 0.707, 0.707, 0.707, 3.536 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 0, 4, 0, 0, 0, 0, 0, 4 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 0, 4, 0, 0, 0, 0, 0, 4 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 0.707, 3.536, 0.707, 0.707, 0.707, 0.707, 0.707, 3.536 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 1.707, 2.55, 1.225, 0.707, 0.293, 0.707, 1.225, 2.55 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+	{
+		auto win = spec.pop();
+		auto mag = get_magnitudes(win);
+		std::array<double, FFT_LEN> exptected = { 2.414, 2, 1, 0, 0.414, 0, 1, 2 };
+		ASSERT_EQ(mag.size(), exptected.size());
+		ASSERT_ARRAYS_EQ(mag, exptected, exptected.size(), 1e-9);
+	}
+
+	ASSERT_ANY_THROW(spec.pop());
 }
 
 #endif
