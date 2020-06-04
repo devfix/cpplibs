6b00c8c95aad20abb81156396da483912c70142b
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2020-06-04 17:04:48 +0200

    final fft test with flattop and normalization

--- devfix/dsp/fft.h
+++ devfix/dsp/fft.h
@@ -6,6 +6,7 @@
 
 #include <complex>
 #include <cmath>
+#include <cstring>
 #include "../base/math.h"
 #include "window.h"
 
@@ -16,7 +17,7 @@ namespace devfix::dsp
 		using math = devfix::base::math;
 
 		template<typename FloatT>
-		static void transform_inplace(std::complex<FloatT>* field, std::size_t len)
+		static void transform_inplace(std::complex<FloatT>* win, std::size_t len)
 		{
 			double theta = M_PI / len;
 			std::complex<FloatT> phi = std::complex<FloatT>(cos(theta), -sin(theta)), w;
@@ -29,9 +30,9 @@ namespace devfix::dsp
 					for (std::size_t a = pair; a < len; a += stage)
 					{
 						std::size_t b = a + stage / 2;
-						std::complex<FloatT> t = field[a] - field[b];
-						field[a] += field[b];
-						field[b] = t * w;
+						std::complex<FloatT> t = win[a] - win[b];
+						win[a] += win[b];
+						win[b] = t * w;
 					}
 					w *= phi;
 				}
@@ -42,44 +43,67 @@ namespace devfix::dsp
 				std::size_t b = math::reverse_bits(a, math::popcount(len));
 				if (b > a)
 				{
-					std::complex<FloatT> t = field[a];
-					field[a] = field[b];
-					field[b] = t;
+					std::complex<FloatT> t = win[a];
+					win[a] = win[b];
+					win[b] = t;
 				}
 			}
 		}
 
 		template<typename FloatT>
-		static void transform_inplace(std::vector<std::complex<FloatT>>& vec)
+		static void transform_inplace(std::vector<std::complex<FloatT>>& win)
 		{
-			transform_inplace<FloatT>(vec.data(), vec.size());
+			transform_inplace<FloatT>(win.data(), win.size());
 		}
 
 		template<typename FloatT, std::size_t N>
-		static void transform_inplace(std::array<std::complex<FloatT>, N>& arr)
+		static void transform_inplace(std::array<std::complex<FloatT>, N>& win)
 		{
 			static_assert(N == 1 << math::popcount(N), "length is not a power of 2");  // assert N is power of 2
-			transform_inplace<FloatT>(arr.data(), arr.size());
+			transform_inplace<FloatT>(win.data(), win.size());
 		}
 
 		template<typename FloatT, FloatT(* win_fun)(std::size_t, std::size_t), std::size_t N>
-		static void apply_window(std::complex<FloatT>* field)
+		static void apply_window(std::complex<FloatT>* win)
 		{
-			for (std::size_t i = 0; i < N; i++) { field[i] *= window::get_window<FloatT, win_fun>(N, i); }
+			for (std::size_t i = 0; i < N; i++) { win[i] *= window::get_window<FloatT, win_fun>(N, i); }
 		}
 
 		template<typename FloatT, FloatT(* win_fun)(std::size_t, std::size_t), std::size_t N>
-		static void apply_window(std::vector<std::complex<FloatT>>& vec)
+		static void apply_window(std::vector<std::complex<FloatT>>& win)
 		{
-			if(vec.size() != N)
-			{throw std::invalid_argument("invalid vector size");}
-			apply_window<FloatT, win_fun, N>(vec.data());
+			if (win.size() != N) { throw std::invalid_argument("invalid vector size"); }
+			apply_window<FloatT, win_fun, N>(win.data());
 		}
 
 		template<typename FloatT, FloatT(* win_fun)(std::size_t, std::size_t), std::size_t N>
-		static void apply_window(std::array<std::complex<FloatT>, N>& arr)
+		static void apply_window(std::array<std::complex<FloatT>, N>& win)
 		{
-			apply_window<FloatT, win_fun, N>(arr.data());
+			apply_window<FloatT, win_fun, N>(win.data());
+		}
+
+		template<typename FloatT>
+		static void normalize_amplitude(const std::complex<FloatT>* win, std::size_t len, FloatT* mag)
+		{
+			mag[0] = std::abs<FloatT>(win[0] * (1. / len));
+			for (std::size_t i = 1; i < len / 2; i++) { mag[i] = std::abs<FloatT>(win[i] * (2. / len)); }
+			mag[len / 2] = std::abs<FloatT>(win[len / 2] * (1. / len));
+		}
+
+		template<typename FloatT>
+		static std::vector<FloatT> normalize_amplitude(const std::vector<std::complex<FloatT>>& win)
+		{
+			std::vector<FloatT> mag(win.size() / 2 + 1);
+			normalize_amplitude(win.data(), win.size(), mag.data());
+			return mag;
+		}
+
+		template<typename FloatT, std::size_t N>
+		static std::array<FloatT, N> normalize_amplitude(const std::array<std::complex<FloatT>, N>& win)
+		{
+			std::array<FloatT, N> mag(win.size() / 2 + 1);
+			normalize_amplitude(win.data(), win.size(), mag.data());
+			return mag;
 		}
 	};
 }
--- devfix/dsp/test/test_fft.cpp
+++ devfix/dsp/test/test_fft.cpp
@@ -12,6 +12,8 @@
 using namespace devfix::dsp;
 
 constexpr double ABS_DOUBLE_ERROR = 1e-12;
+constexpr double ABS_COARSE_ERROR = 1e-3;
+
 constexpr std::size_t LEN = 1024;
 
 template<int digits, typename T>
@@ -23,8 +25,8 @@ TEST(FFT, MultipleFreqs)
 	for (std::size_t i = 0; i < vec.size(); i++)
 	{
 		vec[i] = (sin(2 * M_PI * double(i) / double(vec.size()) * 4.)
-			+ 0.5 * sin(2 * M_PI * double(i) / double(vec.size()) * 2.)
-			+ 0.25 * sin(2 * M_PI * double(i) / double(vec.size()) * 1.)
+			+ 0.5 * std::sin(2 * M_PI * double(i) / double(vec.size()) * 2.)
+			+ 0.25 * std::sin(2 * M_PI * double(i) / double(vec.size()) * 1.)
 			+ 4);
 	}
 
@@ -82,6 +84,54 @@ TEST(FFT, ApplyWindowArray)
 	};
 	ASSERT_EQ(mag.size(), expected.size());
 	for (std::size_t i = 0; i < mag.size(); i++) { ASSERT_NEAR(mag[i], expected[i], ABS_DOUBLE_ERROR); }
+}
+
+TEST(FFT, AmplitudeNormalization)
+{
+	std::vector<std::complex<double>> vec(128);
+	for (std::size_t i = 0; i < vec.size(); i++)
+	{
+		vec[i] = (sin(2 * M_PI * double(i) / double(vec.size()) * 4.)
+			+ 0.5 * std::sin(2 * M_PI * double(i) / double(vec.size()) * 2.)
+			+ 0.25 * std::sin(2 * M_PI * double(i) / double(vec.size()) * 1.)
+			+ 4);
+	}
+
+	fft::transform_inplace(vec);
+	auto mag = fft::normalize_amplitude(vec);
+
+	std::vector<double> expected =
+		{ 4, 0.25, 0.5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+	ASSERT_EQ(mag.size(), expected.size());
+	for (std::size_t i = 0; i < mag.size(); i++) { ASSERT_NEAR(mag[i], expected[i], ABS_DOUBLE_ERROR); }
+}
+
+TEST(FFT, AmplitudeNormalizationWithFlatTop)
+{
+	constexpr std::size_t FFT_LEN = 128;
+	std::vector<std::complex<double>> vec(FFT_LEN);
+	for (std::size_t i = 0; i < vec.size(); i++)
+	{
+		vec[i] = std::sin(2 * M_PI * double(i) / double(vec.size()) * FFT_LEN / 32)
+			+ 0.5 * std::sin(2 * M_PI * double(i) / double(vec.size()) * FFT_LEN / 8)
+			+ 0.25 * std::sin(2 * M_PI * double(i) / double(vec.size()) * FFT_LEN / 4);
+	}
+
+	fft::apply_window<double, window::flattop, FFT_LEN>(vec);
+	fft::transform_inplace(vec);
+	auto mag = fft::normalize_amplitude(vec);
+
+	std::vector<double> expected =
+		{ 0.00175583, 0.201761, 0.650524, 0.967362, 1.00001, 0.967367, 0.650518, 0.201786, 0.0179323, 3.18503e-05, 1.60198e-05, 2.79171e-05,
+		  0.0089526, 0.100907, 0.325245, 0.483698, 0.499992, 0.483696, 0.325248, 0.100902, 0.00895885, 2.19893e-05, 1.4057e-05, 1.57144e-05,
+		  1.56228e-05, 1.46173e-05, 1.26949e-05, 1.55266e-05, 0.00447605, 0.0504534, 0.162622, 0.241849, 0.249996, 0.241848, 0.162624,
+		  0.0504507, 0.00447977, 1.04789e-05, 6.275e-06, 6.82469e-06, 6.52186e-06, 5.91506e-06, 5.27451e-06, 4.68813e-06, 4.17657e-06,
+		  3.73776e-06, 3.36303e-06, 3.04274e-06, 2.76813e-06, 2.53178e-06, 2.32763e-06, 2.15073e-06, 1.99712e-06, 1.86362e-06, 1.74766e-06,
+		  1.64719e-06, 1.56058e-06, 1.48654e-06, 1.42403e-06, 1.37227e-06, 1.33064e-06, 1.29868e-06, 1.27607e-06, 1.26258e-06, 6.29052e-07,
+		};
+	ASSERT_EQ(mag.size(), expected.size());
+	for (std::size_t i = 0; i < mag.size(); i++) { ASSERT_NEAR(mag[i], expected[i], ABS_COARSE_ERROR); }
 }
 
 #endif
