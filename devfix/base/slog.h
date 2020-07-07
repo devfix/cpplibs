4c091458771e6590992d5145b5c18ae92a5e4b59
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2020-07-07 15:45:18 +0200

    back to variant

--- devfix/base/slog.h
+++ devfix/base/slog.h
@@ -32,18 +32,20 @@ namespace devfix::base
 
 		explicit slog(const write_t& write, const flush_t& flush) { add_sink(write, flush); }
 
+
 		void add_sink(slog* sl)
 		{
-			sinks_.emplace_back([sl](const std::basic_string<CharT>& str) { sl->write(str); }, [sl] { sl->end_text(); });
+			sinks_.push_back({sl});
 		}
 
 		void add_sink(ostream_t* ostream)
 		{
-			sinks_.emplace_back([ostream](const std::basic_string<CharT>& str) { ostream->write(str.data(), str.length()); },
-								[ostream] { ostream->flush(); });
+			sinks_.push_back({ std::make_pair<write_t, flush_t>(
+				[ostream](const std::basic_string<CharT>& str) { ostream->write(str.data(), str.length()); },
+				[ostream] { ostream->flush(); }) });
 		}
 
-		void add_sink(const write_t& write, const flush_t& flush) { sinks_.emplace_back(write, flush); }
+		//void add_sink(const write_t& write, const flush_t& flush) { sinks_.emplace_back(write, flush); }
 
 		slog(const slog& sl) = delete;
 		slog(slog&& sl) = delete;
@@ -86,10 +88,27 @@ namespace devfix::base
 
 		[[nodiscard]] bool is_enabled() { return enabled_; }
 
-	private:
+	protected:
 		void start_text()
 		{
-			if (enabled_) { for (auto&[w, _] : sinks_) { w(CLEAR_LINE); }}
+			if (enabled_)
+			{
+				for (auto& sink : sinks_)
+				{
+					if (std::holds_alternative<slog*>(sink))
+					{
+						std::get<slog*>(sink)->start_text();
+					}
+					else if (std::holds_alternative<ostream_t*>(sink))
+					{
+						std::get<ostream_t*>(sink)->write(CLEAR_LINE, sizeof(CLEAR_LINE) / sizeof(CharT));
+					}
+					else
+					{
+						std::get<0>(std::get<std::pair<write_t, flush_t>>(sink))(CLEAR_LINE);
+					}
+				}
+			}
 		}
 
 		void write(const std::basic_string<CharT>& str)
@@ -104,7 +123,21 @@ namespace devfix::base
 					passed += line.length();
 					line.insert(line.begin(), prefix_.begin(), prefix_.end());
 					if (passed < str.length()) { line += CharT('\n'); }
-					for (auto&[w, _] : sinks_) { w(line); }
+					for (auto& sink : sinks_)
+					{
+						if (std::holds_alternative<slog*>(sink))
+						{
+							std::get<slog*>(sink)->write(line);
+						}
+						else if (std::holds_alternative<ostream_t*>(sink))
+						{
+							std::get<ostream_t*>(sink)->write(line.c_str(), line.length());
+						}
+						else
+						{
+							std::get<0>(std::get<std::pair<write_t, flush_t>>(sink))(line);
+						}
+					}
 					passed++;
 				}
 			}
@@ -112,14 +145,31 @@ namespace devfix::base
 
 		void end_text()
 		{
-			if (enabled_) { for (auto&[_, f] : sinks_) { f(); }}
+			if (enabled_)
+			{
+				for (auto& sink : sinks_)
+				{
+					if (std::holds_alternative<slog*>(sink))
+					{
+						std::get<slog*>(sink)->end_text();
+					}
+					else if (std::holds_alternative<ostream_t*>(sink))
+					{
+						std::get<ostream_t*>(sink)->flush();
+					}
+					else
+					{
+						std::get<1>(std::get<std::pair<write_t, flush_t>>(sink))();
+					}
+				}
+			}
 		}
 
 		static constexpr CharT CLEAR_LINE[] = { '\033', '[', '2', 'K', '\r' };
 
 		static inline std::mutex mutex_;
 
-		std::vector<std::pair<write_t, flush_t>> sinks_;
+		std::vector<std::variant<slog*, std::pair<write_t, flush_t>, ostream_t*>> sinks_;
 		std::basic_string<CharT> prefix_;
 		std::atomic_bool enabled_ = true;
 	};
--- devfix/base/test/test_slog.cpp
+++ devfix/base/test/test_slog.cpp
@@ -89,4 +89,34 @@ TEST_CASE("slog - stx wchar")
 	CHECK(out.str() == L"[abc] test\n\033[2K\r[abc] test\n");
 }
 
+TEST_CASE("slog - cascade")
+{
+	std::stringstream out;
+	slog<char> sl1(&out);
+	sl1.set_prefix("[1]");
+	slog<char> sl2(&sl1);
+	sl2.set_prefix("[2]");
+	slog<char> sl3(&sl2);
+	sl3.set_prefix("[3]");
+
+	sl3 << "test";
+	CHECK(out.str() == "[1][2][3]test");
+
+	out.str("");
+	sl3 << "test\n";
+	CHECK(out.str() == "[1][2][3]test\n");
+
+	out.str("");
+	sl3 << "test\n123";
+	CHECK(out.str() == "[1][2][3]test\n[1][2][3]123");
+
+	out.str("");
+	sl3 << "test\n123\n";
+	CHECK(out.str() == "[1][2][3]test\n[1][2][3]123\n");
+
+	out.str("");
+	sl3 << "test" << stx << "abc";
+	CHECK(out.str() == "[1][2][3]test\033[2K\r[1][2][3]abc");
+}
+
 #endif
