7b519dccd7363bab4d9c49d21b515b4c2a81507a
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2020-08-11 09:32:46 +0200

    found problem

--- testrunner.cpp
+++ testrunner.cpp
@@ -2,14 +2,11 @@
 // Created by core on 06/28/20.
 //
 
-/*#define CATCH_CONFIG_EXTERNAL_INTERFACES
+#define CATCH_CONFIG_EXTERNAL_INTERFACES
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 
 int main(int argc, char* argv[])
 {
 	return Catch::Session().run(argc, argv);
-}*/
-
-#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+}
