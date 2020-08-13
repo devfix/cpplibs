1768b5095c5fca968f6ec1efd4415d20d78edf10
Tristan Krause <tristan.krause@stud.htwk-leipzig.de>
2020-08-13 11:08:00 +0200

    save

--- devfix/dsp/winfun.h
+++ devfix/dsp/winfun.h
@@ -10,7 +10,7 @@
 
 namespace devfix::dsp
 {
-	template <typename FloatT>
+	template<typename FloatT>
 	struct winfun
 	{
 
@@ -19,6 +19,22 @@ namespace devfix::dsp
 			return k < n ? (
 				FloatT(.5) + FloatT(.5) * std::cos(FloatT(2) * base::math::pi * (FloatT(k) - FloatT(.5) * FloatT(n - 1)) / (n - 1))
 			) : 0;
+		}
+
+		static constexpr FloatT flattop_matlab(std::size_t n, std::size_t k)
+		{
+			return get_cosine_win<0.21557895, 0.41663158, 0.277263158, 0.083578947, 0.006947368>(n, k);
+		}
+
+	private:
+
+		template<FloatT... Coeffs>
+		static FloatT get_cosine_win(std::size_t n, std::size_t k)
+		{
+			std::array<FloatT, sizeof...(Coeffs)> coeffs = { Coeffs... };
+			FloatT sum = 0;
+			for (std::size_t i = 0; i < coeffs.size(); i++) { sum += coeffs[i] * std::cos(FloatT(2 * base::math::pi * i * k) / n); }
+			return sum;
 		}
 
 	};
